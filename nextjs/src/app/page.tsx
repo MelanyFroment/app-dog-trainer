da76409a87969dbc97e4adbb96ad7134eb106255
TOP-FDesmet <florian.desmet@topchretien.com>
2025-05-25 21:45:09 +0200

    lint nextjs app

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,11 +1,11 @@
 import Link from 'next/link'
 
 export default function Page() {
-  return (
-    <div>
-      <h1>Home</h1>
-      <Link href="/login">Se connecter</Link>
-      <Link href="/about">About</Link>
-    </div>
-  )
+    return (
+        <div>
+            <h1>Home</h1>
+            <Link href="/login">Se connecter</Link>
+            <Link href="/about">About</Link>
+        </div>
+    )
 }
