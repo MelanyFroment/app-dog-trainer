6d4406a4a88657f1122019d520174fc1d62ca481
melan <melany.from@yahoo.com>
2025-07-17 18:05:44 +0200

    Active tests Jest dans GitHub Actions

--- nextjs/jest.config.ts
+++ nextjs/jest.config.ts
@@ -1,18 +1,19 @@
-import type { Config } from 'jest'
-import nextJest from 'next/jest.js'
+// jest.config.ts
+import nextJest from 'next/jest.js';
 
+// Création du Jest config avec next
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
-})
+});
 
-// Add any custom config to be passed to Jest
-const config: Config = {
-  coverageProvider: 'v8',
+// Fichier de config personnalisé
+const customJestConfig = {
   testEnvironment: 'jsdom',
-  // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-}
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+};
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-export default createJestConfig(config)
+// Export final
+export default createJestConfig(customJestConfig);
