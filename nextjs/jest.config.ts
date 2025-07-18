7e0abd2f7dab1b5e9433fda5d3be6f0943597f52
melan <melany.from@yahoo.com>
2025-07-18 19:05:21 +0200

    github action + sonarqube nextjs test

--- nextjs/jest.config.ts
+++ nextjs/jest.config.ts
@@ -9,7 +9,12 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 const config: Config = {
   coverageProvider: 'v8',
+  preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 }
